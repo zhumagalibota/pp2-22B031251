142ca83fa33451d8ebbe3bed9481b144ddc684ec
zhumagalibota <zhumagalibota@outlook.com>
2023-01-28 23:38:52 +0600

    while loops

---
+++ tsis2/whileLoops.py
@@ -0,0 +1,30 @@
+#code 1
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+
+#code 2
+i = 1
+while i < 6:
+  print(i)
+  if i == 3:
+    break
+  i += 1
+
+#code 3
+i = 0
+while i < 6:
+  i += 1
+  if i == 3:
+    continue
+  print(i)
+
+#code 4
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+else:
+  print("i is no longer less than 6")
+
