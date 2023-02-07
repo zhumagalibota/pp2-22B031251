abd64a205362e0c5f99e39f39a666f32f49fa230
zhumagalibota <zhumagalibota@outlook.com>
2023-02-08 01:17:09 +0600

    2nd task added

--- tsis3/functions1.py
+++ tsis3/functions1.py
@@ -1,6 +1,15 @@
+#1
 def grams_to_ounces(grams):
     ounces = 28.3495231 * grams
     print(ounces)
 
-grams = int(input())
-grams_to_ounces(grams)
+#grams = int(input())
+#grams_to_ounces(grams)
+
+#2
+def temperature_conversion(F):
+    C = (5 / 9) * (F - 32)
+    print(C)
+
+F = int(input())
+temperature_conversion(F)
