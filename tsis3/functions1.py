af272f3fe88501e8b5363bed6cfe7110b9d11383
zhumagalibota <zhumagalibota@outlook.com>
2023-02-08 01:27:58 +0600

    task 3 added

--- tsis3/functions1.py
+++ tsis3/functions1.py
@@ -11,5 +11,13 @@ def temperature_conversion(F):
     C = (5 / 9) * (F - 32)
     print(C)
 
-F = int(input())
-temperature_conversion(F)
+#F = int(input())
+#temperature_conversion(F)
+
+#3
+def solve(numheads, numlegs):
+    rabbits = 24/2
+    chickens = numheads - rabbits
+    print('chickens:', chickens, 'rabbits:', rabbits)
+
+solve(35, 94)
