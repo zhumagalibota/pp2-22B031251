e14e3dff87a54ad866ecf6fbcc714ed6bf8503c4
zhumagalibota <zhumagalibota@outlook.com>
2023-02-20 21:42:40 +0600

    .

--- tsis4/dates.py
+++ tsis4/dates.py
@@ -1,11 +1,18 @@
 import datetime
 
 #1
-x = datetime.datetime.now()
+'''x = datetime.datetime.now()
 a = datetime.timedelta(5)
 b = x - a
 print("Current date:", x, "\nsubtracted five days:", b)
 
 #2
-'''x = datetime.datetime.now() 
-a = '''
+today = datetime.datetime.now() 
+x = datetime.timedelta(1)
+yesterday = today - x
+tomorrow = today + x
+print("Today:", today.strftime("%A"), 
+      "\nYesterday:", yesterday.strftime("%A"),
+      "\nTomorrow:", tomorrow.strftime("%A"))'''
+
+#3
