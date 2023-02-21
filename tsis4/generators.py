a59b5e86ca567db0d77b92c7cfa186c2d1e03c72
zhumagalibota <zhumagalibota@outlook.com>
2023-02-21 22:38:51 +0600

    .

--- tsis4/generators.py
+++ tsis4/generators.py
@@ -4,12 +4,35 @@ a = (x**2 for x in range(1, n))
 for x in a:
     print(x)
     if x >= n:
-        break'''
+        break
 
 #2
 n = int(input())
-even = (x%2 for x in range(1, n))
-for x in even:
-    print(x)
-    if x >= n:
-        break
+def gen(n):
+    for v in range(n):
+        if v%2==0:
+            yield v
+            v += 1          
+l = list(gen(n))
+print(*gen(n+1), sep=", ")
+
+#3
+def gen(n):
+    for v in range(n):
+        if v%3==0 and v%4==0:
+            yield v
+            v += 1
+for v in gen(100):
+    print(v)'''
+
+#4
+a = int(input())
+b = int(input())
+a = (v**2 for v in range(a, b))
+for v in a:
+    print(v)
+
+
+
+
+    
