8db010aa2b80b7c9c6c5874d29fc029ee422209a
zhumagalibota <zhumagalibota@outlook.com>
2023-03-06 00:44:51 +0600

    .

---
+++ tsis6/builtinfunctions.py
@@ -0,0 +1,23 @@
+#1
+'''
+import math
+a = input().split()
+b = list(map(int, a))
+c = math.prod(b)
+print(c)
+
+#3
+a = input()
+b = ''.join(reversed(a))
+if a == b:
+    print(1)
+else:
+    print(0)
+
+#5
+a = input().split()
+b = tuple(a)
+print(all(b))'''
+
+
+#----------------2 and 4 not done
