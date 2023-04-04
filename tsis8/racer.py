e4f45aa6b9009965197abfe0e3c88268692bfbb0
zhumagalibota <zhumagalibota@outlook.com>
2023-04-05 00:27:34 +0600

    ,

--- tsis8/racer.py
+++ tsis8/racer.py
@@ -6,6 +6,13 @@ pygame.init()
 
 width, height = 400, 600
 speed = 5
+score = 0
+
+font = pygame.font.SysFont("Verdana", 60)
+font_small = pygame.font.SysFont("Verdana", 20)
+game_over = font.render("Game Over", True, (255, 255, 255))
+
+pygame.mixer.music.load("background.wav")
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Street Racer")
 background = pygame.image.load("AnimatedStreet.png")
@@ -19,8 +26,10 @@ class enemy(pygame.sprite.Sprite):
         self.rect.center = (random.randint(40, width-40), 0)
 
     def move(self):
+        global score
         self.rect.move_ip(0, 10)
         if (self.rect.bottom > 600):
+            score += 1
             self.rect.top = 0
             self.rect.center = (random.randint(30, 370), 0)
 
@@ -32,6 +41,10 @@ class player(pygame.sprite.Sprite):
         self.rect.center = (160, 520)
     def move(self):
         pressed_keys = pygame.key.get_pressed()
+        if pressed_keys[K_UP]:
+            self.rect.move_ip(0, -5)
+        if pressed_keys[K_DOWN]:
+            self.rect.move_ip(0, 5)
         if self.rect.left > 0:
             if pressed_keys[K_LEFT]:
                 self.rect.move_ip(-5, 0)
@@ -52,21 +65,32 @@ speed_inc = pygame.USEREVENT + 1
 pygame.time.set_timer(speed_inc, 1000)
 
 running = True
+if running:
+        pygame.mixer.music.play(-1)
+
 while running:
-    screen.blit(background, (0, 0))
     for event in pygame.event.get():
         if event.type == speed_inc:
-            speed += 2
-
+            speed += 0.5
+        
         if event.type == pygame.QUIT:
             running = False
+    
+    screen.blit(background, (0, 0))
+    scores = font_small.render(str(score), True, (255, 255, 255))
+    screen.blit(scores, (10, 10))
 
     for i in all_sprites:
         screen.blit(i.image, i.rect)
         i.move()
     
     if pygame.sprite.spritecollideany(p1, enemies):
+        pygame.mixer.music.pause()
+        pygame.mixer.Sound("crash.wav").play()
+        time.sleep(0.5)
+
         screen.fill((255, 0, 0))
+        screen.blit(game_over, (30, 250))
         pygame.display.update()
         for i in all_sprites:
             i.kill()
