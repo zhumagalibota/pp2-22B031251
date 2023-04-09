c0969e8007b68c0214aff0afb6cb6a4531b45766
zhumagalibota <zhumagalibota@outlook.com>
2023-04-10 03:27:42 +0600

    .

--- tsis8/paint.py
+++ tsis8/paint.py
@@ -1,91 +1,146 @@
+import collections
+
 import pygame
+
+pygame.init()
+clock = pygame.time.Clock()
+WIDTH, HEIGHT = 800, 800
+SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
+BLACK = pygame.Color(0, 0, 0)
+WHITE = pygame.Color(255, 255, 255)
+
+
+# Point = collections.namedtuple('Point', ['x', 'y'])
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+class GameObject:
+    def draw(self):
+        return
+
+    def update(self, current_pos):
+        return
+
+
+class Button(GameObject):
+    def __init__(self):
+        self.size = 40
+        self.rect = pygame.draw.rect(
+            SCREEN,
+            (0, 255, 0),
+            (
+                WIDTH // 2 - self.size // 2,
+                20,
+                self.size,
+                self.size,
+            )
+        )
+
+    def draw(self):
+        pygame.draw.rect(
+            SCREEN,
+            (0, 255, 0),
+            (
+                WIDTH // 2 - self.size // 2,
+                20,
+                self.size,
+                self.size,
+            )
+        )
+
+    def update(self, current_pos):
+        pass
+
+
+class Pen(GameObject):
+    def __init__(self, *args, **kwargs):  # Pen(1, 2, 3, a=4) =>
+        self.points: list[Point, ...] = []  # [(x1, y1), (x2, y2)]
+
+    def draw(self):
+        for idx, point in enumerate(self.points[:-1]):  # range(len(self.points))
+            next_point = self.points[idx + 1]
+            pygame.draw.line(
+                SCREEN,
+                WHITE,
+                start_pos=(point.x, point.y),  # self.points[idx]
+                end_pos=(next_point.x, next_point.y),
+                width=5
+            )
+
+    def update(self, current_pos):
+        self.points.append(Point(*current_pos))  # (x, y) Point((x, y)) => Point(x, y)
+
+
+
+class Rectangle(GameObject):
+    def __init__(self, start_pos, *args, **kwargs): # Rectangle(start_pos=1); Pen(start_pos=1)
+        self.start_pos = Point(*start_pos)
+        self.end_pos = Point(*start_pos)
+
+    def draw(self):
+        start_pos_x = min(self.start_pos.x, self.end_pos.x)  # min(self.start_pos[0], self.end_pos[0])
+        start_pos_y = min(self.start_pos.x, self.end_pos.y)
+
+        end_pos_x = max(self.start_pos.x, self.end_pos.x)
+        end_pos_y = max(self.start_pos.y, self.end_pos.y)
+
+        pygame.draw.rect(
+            SCREEN,
+            WHITE,
+            (
+                start_pos_x, start_pos_y,
+                end_pos_x - start_pos_x,
+                end_pos_y - start_pos_y,
+            ),
+            width=5,
+        )
+
+    def update(self, current_pos):
+        self.end_pos.x, self.end_pos.y = current_pos
+
 
 
 def main():
-    pygame.init()
-    width, height = 500, 500
-    screen  = pygame.display.set_mode((width, height))
-    pygame .display.set_caption("Paint")
-    clock = pygame.time.Clock()
-    
-    radius = 15
-    x = 0
-    y = 0
-    mode = 'blue'
-    points = []
-    
-    while True:
-        
-        pressed = pygame.key.get_pressed()
-        
-        alt_held = pressed[pygame.K_LALT] or pressed[pygame.K_RALT]
-        ctrl_held = pressed[pygame.K_LCTRL] or pressed[pygame.K_RCTRL]
-        
+    running = True
+    game_object = GameObject()
+    active_obj = game_object
+    current_shape = Pen  # current_shape()
+    button = Button()
+    objects = [
+        button
+    ]
+
+    while running:
+        SCREEN.fill(BLACK)
+
         for event in pygame.event.get():
-            
-            # determin if X was clicked, or Ctrl+W or Alt+F4 was used
             if event.type == pygame.QUIT:
-                return
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_w and ctrl_held:
-                    return
-                if event.key == pygame.K_F4 and alt_held:
-                    return
-                if event.key == pygame.K_ESCAPE:
-                    return
-            
-                # determine if a letter key was pressed
-                if event.key == pygame.K_r:
-                    mode = 'red'
-                elif event.key == pygame.K_g:
-                    mode = 'green'
-                elif event.key == pygame.K_b:
-                    mode = 'blue'
-            
+                running = False
+
             if event.type == pygame.MOUSEBUTTONDOWN:
-                if event.button == 1: # left click grows radius
-                    radius = min(200, radius + 1)
-                elif event.button == 3: # right click shrinks radius
-                    radius = max(1, radius - 1)
-            
+                if button.rect.collidepoint(event.pos):
+                    current_shape = Rectangle
+                    # current_shape = button.connected_shape
+                else:
+                    active_obj = current_shape(start_pos=event.pos)
+                    objects.append(active_obj)
+
             if event.type == pygame.MOUSEMOTION:
-                # if mouse moved, add point to list
-                position = event.pos
-                points = points + [position]
-                points = points[-256:]
-                  
-        screen.fill((0, 0, 0))
-        
-        # draw all points
-        i = 0
-        while i < len(points) - 1:
-            drawLineBetween(screen, i, points[i], points[i + 1], radius, mode)
-            i += 1
-        
+                active_obj.update(current_pos=pygame.mouse.get_pos())
+
+            if event.type == pygame.MOUSEBUTTONUP:
+                active_obj = game_object
+
+        for obj in objects:
+            obj.draw()
+
+        clock.tick(30)
         pygame.display.flip()
-        
-        clock.tick(60)
 
-def drawLineBetween(screen, index, start, end, width, color_mode):
-    c1 = max(0, min(255, 2 * index - 256))
-    c2 = max(0, min(255, 2 * index))
-    
-    if color_mode == 'blue':
-        color = (c1, c1, c2)
-    elif color_mode == 'red':
-        color = (c2, c1, c1)
-    elif color_mode == 'green':
-        color = (c1, c2, c1)
-    
-    dx = start[0] - end[0]
-    dy = start[1] - end[1]
-    iterations = max(abs(dx), abs(dy))
-    
-    for i in range(iterations):
-        progress = 1.0 * i / iterations
-        aprogress = 1 - progress
-        x = int(aprogress * start[0] + progress * end[0])
-        y = int(aprogress * start[1] + progress * end[1])
-        pygame.draw.circle(screen, color, (x, y), width)
 
-main()
+if __name__ == '__main__':
+    main()
